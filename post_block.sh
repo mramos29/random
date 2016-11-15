dfd74e699a6693c6bbca2eda3f24b04c88ef0829
Marco Ramos <mramos@MBP-de-Vera-2.lan>
2016-11-15 22:58:03 +0000

    post_block.sh tuning - exclude Cloudflare ips

--- post_block.sh
+++ post_block.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-thold=20
+thold=30
+
+# Exclude Cloudflare ips from being blocked
+cloudflare="CLOUDFLARE"
 
 # Get top 10 ips with POST requests for the last 5000 request (10 minutes aprox.)
-tail -5000 /var/log/httpd/access_log | grep POST | awk '{print $1}' | sort -rn | uniq -c | sort -rn | head -10 | while read -r line; do
+tail -5000 /var/log/httpd/domains/access.log | grep POST | awk '{print $1}' | sort -rn | uniq -c | sort -rn | head -10 | while read -r line; do
   number=`echo $line | awk '{print $1}'`
+
   # If it's above treshold...
   if [ "$number" -gt "$thold" ] 
   then
     ip=`echo $line | awk '{print $2}'`
     iptables -L -n | grep $ip
-    # ...and wasn't blocked yet, we will block it
+    # ...and wasn't blocked yet...
     if [ $? ]
     then 
-      echo "IP $ip with too much posts. Will Block!" >> /tmp/post_block.log
-      /sbin/iptables -I INPUT 1 -p tcp --source $ip -j DROP
+      ripenet=`whois -n -h whois.ripe.net $ip | grep -i netname | awk '{print $2
+}'`
+      arinnet=`whois -n -h whois.arin.net $ip | grep -i netname | awk '{print $2}'`
+      # ...and ip is not from Cloudflare, we will block it!
+      # First, we check RIPE...
+      if ! [[ "$ripenet" == *"$cloudflare"* ]]
+      then 
+        # ...then we check ARIN
+        if ! [[ "$arinnet" == *"$cloudflare"* ]]
+        then
+          echo -n `date ` >> /tmp/post_block.log
+          echo " - IP $ip with too much posts ($number). Will Block!"  >> /tmp/post_block.log
+          /sbin/iptables -I INPUT 1 -p tcp --source $ip -j DROP
+        fi
+      fi
     fi
   fi 
-done
 
+done
